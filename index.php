eb282e913a9f94a25a48a951472b92ce2ec4f19c
naufalnrsmitter20 <x3mnaufalnabilramadhan@gmail.com>
2024-05-08 19:12:11 +0700

    build: Install Dependencies TailwindCSS and Init in php Projects

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD Perusahaan</title>
+    <link rel="stylesheet" href="src/output.css">
 </head>
 
 <body>
-
+    <main class="">
+        <p class="text-red-600">Hello World</p>
+    </main>
 </body>
 
 </html>
